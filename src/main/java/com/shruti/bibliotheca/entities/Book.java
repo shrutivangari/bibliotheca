14b9758407d3cf4340cb0d4ba5826261bb6574b6
Vangari, Shruti <Shruti.Vangari@nordstrom.com>
2019-09-25 13:43:01 -0700

    H2 DB + 4 inserts

--- src/main/java/com/shruti/bibliotheca/entities/Book.java
+++ src/main/java/com/shruti/bibliotheca/entities/Book.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
 
     private final String bookName;
