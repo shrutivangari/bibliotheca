dd25fa884c107134b0eafcfb41b2f9db81917986
Vangari, Shruti <Shruti.Vangari@nordstrom.com>
2019-09-25 13:53:26 -0700

    Ability to get books

--- src/main/java/com/shruti/bibliotheca/controllers/LibController.java
+++ src/main/java/com/shruti/bibliotheca/controllers/LibController.java
@@ -1,14 +1,31 @@
 package com.shruti.bibliotheca.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.shruti.bibliotheca.entities.Book;
+import com.shruti.bibliotheca.repositories.LibRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:8080")
 public class LibController {
 
+    @Autowired
+    private LibRepository libRepository;
 
     @GetMapping("/test")
     public String getTest() {
         return "Test";
+    }
+
+    @GetMapping("books")
+    public List<Book> getBooks() {
+        return (List<Book>) libRepository.findAll();
+    }
+
+    @PostMapping("books")
+    void addBook(@RequestBody Book book) {
+        libRepository.save(book);
     }
 }
